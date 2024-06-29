a1d674e72c24770218bb1cc085c09b812aa5eb0c
Lucas Bruckbauer <lucas.bruckbauer@gmail.com>
2024-06-29 15:58:02 +0100

    Added: 3.Longest Substring Without Repeating Characters

--- sliding_window/m3_longest_substring_without_repeating_charachters/m3_longest_substring_without_repeating_characters.cpp
+++ sliding_window/m3_longest_substring_without_repeating_charachters/m3_longest_substring_without_repeating_characters.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+/* 
+<-Description->
+    Given a string s, find the length of the longest substring without repeating characters.
+
+<-Constraints->
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+*/
+
 
 class Solution {
 public:
